a5036ffa631b394e7d02e08acb09d1e403505274
444b <15311579+444B@users.noreply.github.com>
2022-10-31 18:04:25 +0000

    creds.db now has some default testing values inside

--- db.py
+++ db.py
@@ -5,16 +5,16 @@ hashed_usrname = "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846
 # sha256 of 5678
 hashed_passwd = "f8638b979b2f4f793ddb6dbd197e0ee25a7a6ea32b0ae22f5e3c5d119d839e75"
 
-   # db setup
-    engine = db.create_engine("sqlite:///creds.db")
-    conn = engine.connect()
-    metadata = db.MetaData()
-    # tables setup
-    members = db.Table('members', metadata,
-                db.Column('usrname_col', db.CHAR(64),primary_key=True),
-                db.Column('passwd_col', db.CHAR(64), nullable=False),
-              )
-    metadata.create_all(engine)
+# db setup
+engine = db.create_engine("sqlite:///creds.db")
+conn = engine.connect()
+metadata = db.MetaData()
+# tables setup
+members = db.Table('members', metadata,
+          db.Column('usrname_col', db.CHAR(64),primary_key=True),
+          db.Column('passwd_col', db.CHAR(64), nullable=False),
+          )
+metadata.create_all(engine)
 
 
 # insert new member (still in testing)
@@ -31,9 +31,11 @@ def insert(entered_usrname, entered_passwd):
         return
 
 
-def db_query(hash, category):
-    output = conn.execute(f"SELECT EXISTS(SELECT 1 FROM members WHERE {category}={hash} LIMIT 1)")
+def db_query(hash):
+    # output = conn.execute(f"SELECT EXISTS(SELECT 1 FROM members WHERE {category}={hash} LIMIT 1)")
+    output = conn.execute(f"SELECT * FROM members WHERE usrname_col = {hash}")
     print(output.fetchall())
+    print(f"{hash} is in the DB :)")
     # result = output.fetchall()
     # if result == str(hash):
     #     return True
@@ -42,5 +44,5 @@ def db_query(hash, category):
 
 
 if __name__ == '__main__':
-    insert(hashed_usrname, hashed_passwd)
-    db_query("03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4", usrname_col)
+    # insert(hashed_usrname, hashed_passwd)
+    db_query("03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4")
