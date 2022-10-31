b337e368f188c86f73aa43d66a70c73212575529
444B <15311579+444B@users.noreply.github.com>
2022-10-31 21:32:59 +0200

    Update db.py
    Confirmed working, 
    - can insert with insert(entered_usrname, entered_passwd)
    - can query to True or False with db_query(hash, category)

--- db.py
+++ db.py
@@ -1,10 +1,5 @@
 import sqlalchemy as db
 
-# sha256 of 1234
-hashed_usrname = "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4"
-# sha256 of 5678
-hashed_passwd = "f8638b979b2f4f793ddb6dbd197e0ee25a7a6ea32b0ae22f5e3c5d119d839e75"
-
 # db setup
 engine = db.create_engine("sqlite:///creds.db")
 conn = engine.connect()
@@ -15,7 +10,6 @@ members = db.Table('members', metadata,
           db.Column('passwd_col', db.CHAR(64), nullable=False),
           )
 metadata.create_all(engine)
-
 
 # insert new member (still in testing)
 def insert(entered_usrname, entered_passwd):
@@ -30,19 +24,35 @@ def insert(entered_usrname, entered_passwd):
         print("No values were inserted.")
         return
 
+# category types are queried_usrname, queried_passwd
+def db_query(hash, category):
+    if category == "queried_usrname":
+        output = conn.execute(
+            f"SELECT EXISTS(SELECT 1 FROM members WHERE usrname_col = ? LIMIT 1)", (hash,)
+            )
+        result = output.fetchall()
+        if result[0][0] == 1:
+            print("is 1")
+            return True
+        else:
+            return False
 
-def db_query(hash):
-    # output = conn.execute(f"SELECT EXISTS(SELECT 1 FROM members WHERE {category}={hash} LIMIT 1)")
-    output = conn.execute(f"SELECT * FROM members WHERE usrname_col = {hash}")
-    print(output.fetchall())
-    print(f"{hash} is in the DB :)")
-    # result = output.fetchall()
-    # if result == str(hash):
-    #     return True
-    # else:
-    #     return False
-
+    if category == "queried_passwd":
+        output = conn.execute(
+            f"SELECT EXISTS(SELECT 1 FROM members WHERE passwd_col = ? LIMIT 1)", (hash,)
+            )
+        result = output.fetchall()
+        if result[0][0] == 1:
+            print("is 1")
+            return True
+        else:
+            return False
+    else:
+        print("Malformed Query category error")
 
 if __name__ == '__main__':
     # insert(hashed_usrname, hashed_passwd)
-    db_query("03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4")
+    if db_query("03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4", "queried_usrname") == True:
+        print("TRUEEEE")
+    else:
+        print("FALSE")
