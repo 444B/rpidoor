6143bb6f675bc5756229efd338b3e6416c9b20f4
444b <15311579+444B@users.noreply.github.com>
2022-11-02 18:28:11 +0000

    ready for testing with copilot

--- door.py
+++ door.py
@@ -1,7 +1,7 @@
 import hashlib
 from time import sleep
 from db import db_query
-# from gpiozero import LED
+from gpiozero import LED
 
 print("\n")
 
@@ -23,37 +23,40 @@ def get_passwd(cleartext_passwd):
 # function to open the door. in testing, just prints
 def open_sesame():
     if usrname_check and passwd_check:
-        print("the door is OPEN\n")
-        print("sleep 15 seconds")
-        # usrname_led.off()
-        # passwd_led.off()
+        usrname_led.on()
+        passwd_led.on()
+        print("Door has been opened")
+        sleep(5)
+        usrname_led.off()
+        passwd_led.off()
+        print("Door has been closed")
+        return
 
 
 # flashes both LEDs
 def flash_both_led(time):
     for num in range(time):
-        print("flashing LED " + str(time) + " times, pulse " + str(num + 1))
-        sleep(1)
-        # usrname_led.on()
-        # passwd_led.on()
-        # sleep(0.5)
-        # usrname_led.off()
-        # passwd_led.off()
-        # sleep(0.5)
+        print(f"Locked out for {time - num} more seconds")
+        usrname_led.on()
+        passwd_led.on()
+        sleep(0.5)
+        usrname_led.off()
+        passwd_led.off()
+        sleep(0.5)
 
 
 # TODO figure out how to add color to this logic
-# def change_led(category, action):
-#     if category == usrname_led:
-#         if action == 1:
-#             usrname_led.on()
-#         if action == 0:
-#             usrname_led.off()
-#     if category == passwd_led:
-#         if action == 1:
-#             passwd_led.on()
-#         if action == 0:
-#             passwd_led.off()
+def change_led(category, action):
+    if category == usrname_led:
+        if action == 1:
+            usrname_led.on()
+        if action == 0:
+            usrname_led.off()
+    if category == passwd_led:
+        if action == 1:
+            passwd_led.on()
+        if action == 0:
+            passwd_led.off()
 
 
 # main program runs from here
@@ -65,10 +68,10 @@ if __name__ == "__main__":
     hashed_passwd = ""
     usrname_check = False
     passwd_check = False
-    # usrname_led = LED(11)
-    # passwd_led = LED(18)
-    # change_led(usrname_led, 0)
-    # change_led(passwd_led, 0)
+    usrname_led = LED(12)
+    passwd_led = LED(5)
+    change_led(usrname_led, 0)
+    change_led(passwd_led, 0)
 
     # running program in a loop
     while True:
@@ -79,13 +82,13 @@ if __name__ == "__main__":
 
             # admin override using magic hash_data
             if hashed_usrname == "7485f7f14090849ddbece011de103c40eadcdc0031885dc29f899f3c5a727428":
-                # change_led(usrname_led, 1)
+                change_led(usrname_led, 1)
                 fuckups = 0
                 usrname_check = True
 
             # compare usrname to creds.db
             if db_query(hashed_usrname, "queried_usrname"):
-                # change_led(usrname_led, 1)
+                change_led(usrname_led, 1)
                 fuckups = 0
                 usrname_check = True
 
@@ -99,7 +102,7 @@ if __name__ == "__main__":
             if fuckups > 2:
                 print("Too many incorrect attempts, pls wait 30s and try again")
                 flash_both_led(30)
-                # sleep(30)
+                sleep(30)
                 fuckups = 0
 
         # passwrd check loop
@@ -108,12 +111,12 @@ if __name__ == "__main__":
 
             # admin override using magic hash_data
             if hashed_passwd == "936867247aef14d232e539bd3f08b2c6bc47afe56a774ede3488d66006cbeb95":
-                # change_led(passwd_led, 1)
+                change_led(passwd_led, 1)
                 passwd_check = True
 
             # compare usrname to creds.db
             if db_query(hashed_passwd, "queried_passwd"):
-                # change_led(passwd_led, 1)
+                change_led(passwd_led, 1)
                 passwd_check = True
 
             # incorrect input
@@ -125,12 +128,14 @@ if __name__ == "__main__":
             # timeout after 3 incorrect input
             if fuckups > 2:
                 print("Too many incorrect attempts, pls wait 30s and try again")
-                flash_both_led(30)
+                flash_both_led(5)
                 fuckups = 0
                 usrname_check = False
 
         # opens the door. TODO Need to write protective logic around this function
         if usrname_check and passwd_check:
             open_sesame()
+            break
 
             # TODO - write a function in db.py that checks that the usrname and passwd match each other
+
