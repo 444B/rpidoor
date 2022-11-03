6888baf5701560fb37d93ae4bca6276921e042a0
Astra <astra@astrra.space>
2022-11-03 19:48:22 +0200

    Add full support for PN532 over I2C, refactor logic

--- door.py
+++ door.py
@@ -1,163 +1,156 @@
 import hashlib
 from time import sleep
 from gpiozero import LED
-from db import db_query, db_query_match
+from db import db_query, db_query_match, db_insert, db_reset
 from nfc_reader import nfc_setup, nfc_loop
 
 
 print("\n")
 
+MAX_ATTEMPTS = 3
+TIMEOUT_SECONDS = 30
+OPEN_SECONDS = 5
+
 
 # hash the username
-def hash_username(cleartext_usrname):
-    hashed_usrname = hashlib.sha256(cleartext_usrname.encode()).hexdigest()
-    return hashed_usrname
+def hash_username(cleartext_username):
+    hashed_username = hashlib.sha256(cleartext_username.encode()).hexdigest()
+    return hashed_username
+
+# hash the password
+def hash_password(cleartext_password):
+    hashed_password = hashlib.sha256(cleartext_password.encode()).hexdigest()
+    return hashed_password
 
 
-# get passwd and save as a global string(hashed)
-def get_passwd(cleartext_passwd):
-    global hashed_passwo
-    hashed_passwd = hashlib.sha256(cleartext_passwd.encode()).hexdigest()
-    return hashed_passwd
-
-
-# function to open the door. in testing, just prints
-def open_sesame():
-    usrname_led.on()
-    passwd_led.on()
+# function to open/close the door. in testing, just prints
+def open_door():
+    username_led.on()
+    password_led.on()
     print("Door has been opened")
-    return
 
-def close_sesame():
+def close_door():
     print("Door has been closed")
-    usrname_led.off()
-    passwd_led.off()
-    return
+    username_led.off()
+    password_led.off()
 
 
 # flashes both LEDs
 def flash_both_led(time):
     for num in range(time):
         print(f"Locked out for {time - num} more seconds")
-        usrname_led.on()
-        passwd_led.on()
+        username_led.on()
+        password_led.on()
         sleep(0.5)
-        usrname_led.off()
-        passwd_led.off()
+        username_led.off()
+        password_led.off()
         sleep(0.5)
 
 
 # TODO figure out how to add color to this logic
 def change_led(category, action):
-    if category == usrname_led:
+    if category == username_led:
         if action == 1:
-            usrname_led.on()
+            username_led.on()
         if action == 0:
-            usrname_led.off()
-    if category == passwd_led:
+            username_led.off()
+    if category == password_led:
         if action == 1:
-            passwd_led.on()
+            password_led.on()
         if action == 0:
-            passwd_led.off()
+            password_led.off()
 
 
 # main program runs from here
 if __name__ == "__main__":
 
     # initial variables setup
-    fuckups = 0
-    hashed_usrname = ""
-    hashed_passwd = ""
-    usrname_check = False
-    passwd_check = False
+    attempts = 0
+    hashed_password = ""
+    username_check = False
+    password_check = False
     match_check = False
-    usrname_led = LED(12)
-    passwd_led = LED(5)
-    change_led(usrname_led, 0)
-    change_led(passwd_led, 0)
+    username_led = LED(12)
+    password_led = LED(5)
+    change_led(username_led, 0)
+    change_led(password_led, 0)
+    
 
     # running program in a loop
 
     nfc_setup()
     while True:
 
-        # usrname check loop
-        while not usrname_check:
-            result = nfc_loop()
-            if result:
-                hashed_usrname = hash_username(result)
-            else: 
+        # card UID check loop
+        attempts = 0
+        while not username_check:
+            uid = nfc_loop()
+            if not uid:
                 continue
 
-            # admin override using magic hash_data
-            if hashed_usrname == "7485f7f14090849ddbece011de103c40eadcdc0031885dc29f899f3c5a727428":
-                change_led(usrname_led, 1)
-                fuckups = 0
-                usrname_check = True
+            # compare card UID to creds.db
+            if db_query(uid, "query_username"):
+                change_led(username_led, 1)
+                username_check = True
+                print("Username is correct")
 
-            # compare usrname to creds.db
-            if db_query(hashed_usrname, "queried_usrname"):
-                change_led(usrname_led, 1)
-                fuckups = 0
-                usrname_check = True
+            # incorrect card
+            elif attempts < MAX_ATTEMPTS:
+                attempts += 1
+                print(f"Incorrect card UID, try again.You have {MAX_ATTEMPTS - attempts} attempts left")
+                flash_both_led(1)
+                username_check = False
+
+            # timeout after 3 incorrect cards
+            else:
+                print("Too many incorrect attempts, pls wait {TIMEOUT_SECONDS} seconds and try again")
+                flash_both_led(TIMEOUT_SECONDS)
+                sleep(TIMEOUT_SECONDS)
+                username_check = False
+                attempts = 0
+
+        # password check loop
+        attempts = 0
+        while not password_check:
+            hashed_password = hash_password(input("Please enter your password: "))
+
+            # compare password to creds in the db
+            if db_query(hashed_password, "query_password"):
+                print("Password is correct")
+                change_led(password_led, 1)
+                password_check = True
 
             # incorrect input
-            else:
-                fuckups += 1
-                print(f"Incorrect user, try again.You have {3 - fuckups} attempts left")
+            elif attempts < MAX_ATTEMPTS:
+                attempts += 1
+                print(f"Incorrect password, try again.You have {MAX_ATTEMPTS - attempts} attempts left")
                 flash_both_led(1)
-                usrname_check = False
+                password_check = False
 
-            # timeout after 3 incorrect input
-            if fuckups > 2:
-                print("Too many incorrect attempts, pls wait 30s and try again")
-                flash_both_led(30)
-                sleep(30)
-                fuckups = 0
-
-        # passwrd check loop
-        while not passwd_check:
-            get_passwd(input("Please enter your passwd\n"))
-
-            # admin override using magic hash_data
-            if hashed_passwd == "936867247aef14d232e539bd3f08b2c6bc47afe56a774ede3488d66006cbeb95":
-                change_led(passwd_led, 1)
-                passwd_check = True
-
-            # compare usrname to creds.db
-            if db_query(hashed_passwd, "queried_passwd"):
-                change_led(passwd_led, 1)
-                passwd_check = True
-
-            # incorrect input
+            # timeout after MAX_ATTEMPTS of incorrect inputs
             else:
-                fuckups += 1
-                print(f"Incorrect passwd, try again.You have {3 - fuckups} attempts left")
-                flash_both_led(1)
-                passwd_check = False
-                usrname_check = False
-
-            # timeout after 3 incorrect input
-            if fuckups > 2:
-                print("Too many incorrect attempts, pls wait 30s and try again")
+                print("Too many incorrect attempts, pls wait {TIMEOUT_SECONDS} seconds and try again")
                 flash_both_led(5)
-                fuckups = 0
-                usrname_check = False
+                attempts = 0
+                username_check = False
+                password_check = False
 
-            # check that usrname and passwd match
-            if db_query_match(hashed_usrname, hashed_passwd):
-                match_check = True
-            else:
-                print("Username and password do not match")
-                passwd_check = False
-                fuckups += 1
-
-        # opens the door. TODO Need to write protective logic around this function
-        if usrname_check and passwd_check and match_check:
-            open_sesame()
-            sleep(5)
-            close_sesame()
-            usrname_check = False
-            passwd_check = False
+        # check that username and password match
+        if db_query_match(uid, hashed_password):
+            match_check = True
+        else:
+            print("Username and password do not match. Try again.")
+            password_check = False
+            username_check = False
             match_check = False
-            fuckups = 0
+            attempts = 0
+
+        # opens the door. 
+        if username_check and password_check and match_check:
+            open_door()
+            sleep(OPEN_SECONDS)
+            close_door()
+            username_check = False
+            password_check = False
+            match_check = False
+            attempts = 0
